d98e041cc3f7c460c0a8cbd9a0cb37a5551bca10
DESKTOP-Q9GD575\PC <thehaodev96@gmail.com>
2024-07-01 07:25:57 +0700

    Exercise - Streamlit and Gradio - Fix Duplicate code

--- module_1/project/streamlit/object_detection/object_detection.py
+++ module_1/project/streamlit/object_detection/object_detection.py
@@ -1,32 +1,7 @@
-import cv2
 import numpy as np
 from PIL import Image
 import streamlit as st
-
-MODEL = "D:/AI_VIETNAM/CODE_EXERCISE/AIO-Exercise/module_1/project/model/MobileNetSSD_deploy.caffemodel"
-PROTOTXT = "D:/AI_VIETNAM/CODE_EXERCISE/AIO-Exercise/module_1/project/model/MobileNetSSD_deploy.prototxt.txt"
-
-
-def process_image(image):
-    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
-    net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
-    net.setInput(blob)
-    detections = net.forward()
-
-    return detections
-
-
-def annotate_image(image: Image, detections, confidence_threshold=0.5):
-    (h, w) = image.shape[:2]
-    for i in np.arange(0, detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-
-        if confidence > confidence_threshold:
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (start_x, start_y, end_x, end_y) = box.astype("int")
-            cv2.rectangle(image, (start_x, start_y), (end_x, end_y), 70, 2)
-
-    return image
+from module_1.project.utils.utils_object_detection import process_image, annotate_image
 
 
 def run():
--- module_1/project/streamlit/word_correction/word_correction.py
+++ module_1/project/streamlit/word_correction/word_correction.py
@@ -1,43 +1,5 @@
 import streamlit as st
-
-
-def find_levenshtein_distance(source: str, target: str):
-    # Create matrix with two space insert
-    first_row = [_ for _ in f"  {source}"]
-    first_col = [_ for _ in f"  {target}"]
-    rows: int = len(first_col)
-    cols: int = len(first_row)
-    matrix = [[0] * cols for _ in range(rows)]
-
-    # Fill the first row with source char and first column with target char
-    for c in range(cols):
-        matrix[0][c] = first_row[c]
-    for r in range(rows):
-        matrix[r][0] = first_col[r]
-
-    # Numbering the initial transformations
-    for i in range(2, cols):
-        matrix[1][i] = i-1
-    for i in range(2, rows):
-        matrix[i][1] = i-1
-
-    # Numbering the transformations to trans from source to target
-    for r in range(2, rows):
-        for c in range(2, cols):
-            sub_cost = 0
-            if matrix[0][c] != matrix[r][0]:
-                sub_cost = 1
-            matrix[r][c] = min(matrix[r][c-1] + 1, matrix[r-1][c] + 1, matrix[r-1][c-1] + sub_cost)
-
-    return matrix[rows-1][cols-1]
-
-
-def load_vocab(file_path):
-    with open(file_path, "r") as f:
-        lines = f.readlines()
-        words = sorted(set([line.strip().lower() for line in lines]))
-
-        return words
+from module_1.project.utils.utils_word import find_levenshtein_distance, load_vocab
 
 
 def run():
